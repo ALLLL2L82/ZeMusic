7af1fec7ccc08b5cfcfcd00c9d5cb0353cecc103
ALLLL2L82 <ynynyghyty@gmail.com>
2024-05-19 17:46:06 +0200

    Update ميمز.py

--- ZeMusic/plugins/play/ميمز.py
+++ ZeMusic/plugins/play/ميمز.py
@@ -8,13 +8,13 @@ from ZeMusic import app
 @app.on_message(filters.command(["صرصار"], ""))
 def mody(client, message):
     message.reply_photo(
-        photo="https://graph.org/file/64b768cff9c90461692d5.jpg",
+        photo="https://telegra.ph/file/018bff5d227366a421b6f.jpg",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("《🪳》", callback_data="mody")]])
     )
 
 @app.on_callback_query(filters.command("mody"))
 def modyy(client, callback_query):
-    video = "https://graph.org/file/fb6ae3a43f73ef2aee8a9.mp4"
+    video = "https://telegra.ph/file/018bff5d227366a421b6f.jpg"
     callback_query.edit_message_media(
         media=InputMediaVideo(media=video, caption=f"⚡هو المعفن اللى صحي الصرصار يجماعه😂👇\n\n{callback_query.from_user.first_name}")
     )
@@ -23,13 +23,13 @@ def modyy(client, callback_query):
 @app.on_message(filters.command(["خنزير"], ""))
 def zeqe(client, message):
     message.reply_photo(
-        photo="https://graph.org/file/c6234a6aedfbe638e0683.jpg",
+        photo="https://telegra.ph/file/018bff5d227366a421b6f.jpg",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("《🐖》", callback_data="modyl")]])
     )
 
 @app.on_callback_query(filters.command("modyl"))
 def zeqep(client, callback_query):
-    video = "https://graph.org/file/274b6971aeb298bdcd6fe.mp4"
+    video = "https://telegra.ph/file/018bff5d227366a421b6f.jpg"
     callback_query.edit_message_media(
         media=InputMediaVideo(media=video, caption=f"⚡هو المعفن اللى صحي الخنزير يجماعه😂👇\n\n{callback_query.from_user.first_name}")
     )    
@@ -38,13 +38,13 @@ def zeqep(client, callback_query):
 @app.on_message(filters.command(["نمله"], ""))
 def namlo(client, message):
     message.reply_photo(
-        photo="https://graph.org/file/bd1024b2f29996675596d.jpg",
+        photo="https://telegra.ph/file/018bff5d227366a421b6f.jpg",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("《🦗》", callback_data="modyll")]])
     )
 
 @app.on_callback_query(filters.command("modyll"))
 def namlop(client, callback_query):
-    video = "https://graph.org/file/2d20cb201e06612588136.mp4"
+    video = "https://telegra.ph/file/018bff5d227366a421b6f.jpg"
     callback_query.edit_message_media(
         media=InputMediaVideo(media=video, caption=f"⚡هو المعفن اللى صحي النمله يجماعه😂👇\n\n{callback_query.from_user.first_name}")
     )        
